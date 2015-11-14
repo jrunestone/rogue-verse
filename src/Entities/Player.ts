91fefb506390cfe144a6e97d7cdb3f5aef489691
Johan Johansson <Johan Johansson>
2015-11-14 16:38:29 +0100

    Moved actual thrust manipulation to ship

--- src/Entities/Player.ts
+++ src/Entities/Player.ts
@@ -5,24 +5,40 @@ namespace RogueVerse.Entities {
     export class Player {
         game: Phaser.Game;
         ship: Entities.Ship;
-        cursors: Phaser.CursorKeys;
-        
+        controls: { forward: Phaser.Key, reverse: Phaser.Key, left: Phaser.Key, right: Phaser.Key };
+
         constructor(game: Phaser.Game, ship: Entities.Ship) {
             this.ship = ship;
             this.game = game;
-            
+
             this.game.add.existing(this.ship);
             this.game.physics.p2.enable(this.ship);
-            
-            this.cursors = this.game.input.keyboard.createCursorKeys();
+
+            this.controls = this.game.input.keyboard.addKeys({ 'forward': Phaser.KeyCode.W, 'reverse': Phaser.KeyCode.S, 'left': Phaser.KeyCode.A, 'right': Phaser.KeyCode.D });
+        }
+
+        update() {
+            if (this.controls.forward.isDown) {
+                this.ship.thrustForward();
+            } else if (this.controls.reverse.isDown) {
+                this.ship.thrustReverse();
+            }
+
+            this.ship.yaw(this.getRotationToPointer());
         }
         
-        update() {
-            if (this.cursors.up.isDown) {
-                this.ship.body.thrust(400);
-            } else if (this.cursors.down.isDown) {
-                this.ship.body.reverse(400);
+        private getRotationToPointer() {
+            var angleToPointer = Phaser.Math.angleBetween(this.ship.x, this.ship.y, this.game.input.activePointer.x, this.game.input.activePointer.y);
+            var deltaMouseRad = this.ship.rotation - angleToPointer - Math.PI / 2;
+  
+            var mod = Math.PI * 2;
+            deltaMouseRad = deltaMouseRad % mod; 
+  
+            if (deltaMouseRad != deltaMouseRad % (mod / 2)) {
+                deltaMouseRad = (deltaMouseRad < 0) ? deltaMouseRad + mod : deltaMouseRad - mod;
             }
+            
+            return deltaMouseRad;
         }
     }
 }
--- src/Entities/Ship.ts
+++ src/Entities/Ship.ts
@@ -2,8 +2,23 @@
 
 namespace RogueVerse.Entities {
     export class Ship extends Phaser.Sprite {
+        thrust = 400;
+        turnRate = 50;
+        
         constructor(game: Phaser.Game, key: string) {
             super(game, game.world.centerX, game.world.centerY, key);
+        }
+        
+        thrustForward() {
+            this.body.thrust(this.thrust);
+        }
+        
+        thrustReverse() {
+            this.body.reverse(this.thrust);
+        }
+        
+        yaw(angle: number) {
+            this.body.rotateLeft(this.turnRate * angle);
         }
     }
 }
