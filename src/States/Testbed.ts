8ce9de2ea2ea8ec87304033af640b23bb4be7495
Johan Johansson <Johan Johansson>
2015-11-15 03:05:41 +0100

    Parallax background scrolling

--- src/States/Testbed.ts
+++ src/States/Testbed.ts
@@ -9,12 +9,15 @@ namespace RogueVerse.States {
         
         player: Entities.Player;
         hud: Components.Hud;
+        
+        starField: Phaser.TileSprite;
+        nebulaField: Phaser.TileSprite;
         // lights: Phaser.Filter;
         
         create() {
-            var starField = game.add.tileSprite(0, 0, 2000, 2000, "starfield");
-            var nebulaField = game.add.tileSprite(0, 0, 2000, 2000, "nebulafield");
-            nebulaField.alpha = 0.3;
+            this.starField = game.add.tileSprite(0, 0, 2000, 2000, "starfield");
+            this.nebulaField = game.add.tileSprite(0, 0, 2000, 2000, "nebulafield");
+            this.nebulaField.alpha = 0.3;
             
             var ship = new Entities.Ship(this.game, "ship1");
             
@@ -26,12 +29,21 @@ namespace RogueVerse.States {
             
             // this.lights = new Phaser.Filter(this.game, null, this.game.cache.getShader("lights"));
             // this.lights.addToWorld(0, 0, 1280, 960);
+            
+            this.starField.autoScroll(10, 0);
         }
         
         update() {
             this.player.update();
             this.hud.update();
             // this.lights.update();
+            
+            if (this.player.ship.getTotalSpeed() > 0) {
+                this.nebulaField.tilePosition.x -= this.player.ship.body.velocity.x * 0.001;
+                this.nebulaField.tilePosition.y -= this.player.ship.body.velocity.y * 0.001;
+                this.starField.tilePosition.x -= this.player.ship.body.velocity.x * 0.01;
+                this.starField.tilePosition.y -= this.player.ship.body.velocity.y * 0.01;
+            }
         }
     }
 }
